8528822f49e37734a0b2e772c34b105b189cc4bb
gtheler <jeremy@seamplex.com>
2021-06-08 17:45:28 -0300

    vector test

---
+++ tests/vector.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+for i in . tests; do
+  if [ -e ${i}/functions.sh ]; then
+    . ${i}/functions.sh 
+  fi
+done
+if [ -z "${functions_found}" ]; then
+  echo "could not find functions.sh"
+  exit 1;
+fi
+
+answer fibo_vector.fee "1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025"
+exitifwrong $?
