1b5752167c64a435ddf3181ad7f2f4d3cf088273
gtheler <jeremy@seamplex.com>
2021-07-13 09:01:51 -0300

    do not remove gsl directory in autogen

--- autoclean.sh
+++ autoclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # 
 # Execute this script to clean the directory and leave it as
-# a fresh mercurial branch
+# a fresh git repository branch
 #
 if test -e Makefile; then
   make clean
 fi
 rm -f *~ .*~ src/*~
 rm -f src/variables.mak
-cat .gitignore | grep -v nbproject | sed '/^#.*/ d' | sed '/^\s*$/ d' | sed 's/^/rm -rf /' | bash
+cat .gitignore | grep -v nbproject | grep -v gsl | sed '/^#.*/ d' | sed '/^\s*$/ d' | sed 's/^/rm -rf /' | bash
