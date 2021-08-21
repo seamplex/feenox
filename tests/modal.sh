79068368dc01b628bd5accca3b0cda572a1ca793
gtheler <jeremy@seamplex.com>
2021-08-21 08:35:53 -0300

    free-free vibration tests

--- tests/modal.sh
+++ tests/modal.sh
@@ -25,3 +25,22 @@ exitifwrong $?
 
 answer1 beam-cantilever-modal.fee hex27 "68.5 169.2 450.2 1037.6 1098.9 1384.2"
 exitifwrong $?
+
+
+
+answer1 beam-cantilever-modal-free-free.fee tet4 "1570.3 2248.0 5220.3 5395.0 6244.1 9072.3"
+exitifwrong $?
+
+answer1 beam-cantilever-modal-free-free.fee tet10 "419.5 1075.9 1265.7 2243.0 2977.0 3151.3"
+exitifwrong $?
+
+# TODO: this one has a nan
+answer1 beam-cantilever-modal-free-free.fee hex8 "1001.3 1805.9 2190.3 2834.6 -nan 4850.2"
+exitifwrong $?
+
+# TODO: this one has a complex eigenvalue
+# answer1 beam-cantilever-modal-free-free.fee hex20 "69.3 169.6 456.3 1040.7 1101.3 1411.5"
+# exitifwrong $?
+
+answer1 beam-cantilever-modal-free-free.fee hex27 "373.9 796.8 1076.3 1806.1 2139.9 2458.0"
+exitifwrong $?
