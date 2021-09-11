00d01d913a03301b011adf30d71d2272c3587cd6
gtheler <jeremy@seamplex.com>
2021-09-11 11:13:39 -0300

    missing $dir

--- tests/iaea-pwr.sh
+++ tests/iaea-pwr.sh
@@ -12,10 +12,10 @@ fi
 checkgmsh
 checkslepc
 
-gmsh -v 0 -2 2dpwr-quarter.geo
+gmsh -v 0 -2 ${dir}/2dpwr-quarter.geo
 answer1 2dpwr.fee quarter "1.02986"
 exitifwrong $?
 
-gmsh -v 0 -2 2dpwr-eighth.geo
+gmsh -v 0 -2 ${dir}/2dpwr-eighth.geo
 answer1 2dpwr.fee eighth  "1.02975"
 exitifwrong $?
