f155551176304d26c2684ed1497fcef1cfb114b7
gtheler <jeremy@seamplex.com>
2021-10-22 14:18:30 -0300

    run

--- tests/nafems/le10-aster-lizard/run.sh
+++ tests/nafems/le10-aster-lizard/run.sh
@@ -44,7 +44,9 @@ fi
 
 # TODO: --check
 
-for c in 1 0.5; do
+# for c in 1 0.5; do
+for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0.14 0.135 0.13 0.1275 0.125 0.1225 0.12 0.118 0.116 0.115 0.114 0.113 0.112 0.11 0.109 0.108 0.107 0.106 0.105 0.104 0.103 0.102 0.101 0.1; do
+
  for m in hex tet; do
 
   if [ ! -e le10-${m}-${c}.msh ]; then
@@ -56,7 +58,7 @@ for c in 1 0.5; do
   if [ ! -z "${has_feenox}" ]; then
     if [ ! -e feenox-${m}-${c}.sigmay ]; then
       echo "running FeenoX GAMG m = ${m} c = ${c}"
-      ${time} -o feenox-${m}-${c}.time feenox le10.fee ${m}-${c} > feenox-${m}-${c}.sigmay || exit 1
+      ${time} -o feenox-${m}-${c}.time feenox le10.fee ${m}-${c} > feenox-${m}-${c}.sigmay
     fi
     
     if [ -e feenox-${m}-${c}.sigmay ]; then
@@ -71,7 +73,7 @@ for c in 1 0.5; do
   if [ ! -z "${has_feenox_mumps}" ]; then
     if [ ! -e feenox_mumps-${m}-${c}.sigmay ]; then
       echo "running FeenoX MUMPS c = ${c}"
-      ${time} -o feenox_mumps-${m}-${c}.time feenox le10.fee ${m}-${c} --mumps > feenox_mumps-${m}-${c}.sigmay || exit 1
+      ${time} -o feenox_mumps-${m}-${c}.time feenox le10.fee ${m}-${c} --mumps > feenox_mumps-${m}-${c}.sigmay
     fi
 
     if [ -e feenox_mumps-${m}-${c}.sigmay ]; then
@@ -88,7 +90,7 @@ for c in 1 0.5; do
     if [ ! -e sparselizard-${m}-${c}.sigmay ]; then
       echo "Running Sparselizard m = ${m} c = ${c}"
       cd sparselizard
-      ${time} -o ../sparselizard-${m}-${c}.time ./run_sparselizard.sh ${m}-${c} > ../sparselizard-${m}-${c}.sigmay || exit 1
+      ${time} -o ../sparselizard-${m}-${c}.time ./run_sparselizard.sh ${m}-${c} > ../sparselizard-${m}-${c}.sigmay
       cd ..
     fi
     
@@ -110,7 +112,7 @@ for c in 1 0.5; do
     if [ ! -e aster-${m}-${c}.sigmay ]; then
       echo "running Aster c = ${c}"
       ln -sf le10_2nd-${m}-${c}.unv le10_2nd.unv
-      ${time} -o aster-${m}-${c}.time as_run le10.export || exit 1
+      ${time} -o aster-${m}-${c}.time as_run le10.export
       grep "degrés de liberté:" message  | awk '{printf("%g\t", $7)}' > aster-${m}-${c}.sigmay
       grep "2.00000000000000E+03  0.00000000000000E+00  3.00000000000000E+02" DD.txt | awk '{print $5}' >> aster-${m}-${c}.sigmay
     fi
@@ -127,7 +129,7 @@ for c in 1 0.5; do
   if [ ! -z "${has_reflex}" ]; then
     if [ ! -e reflex-${m}-${c}.sigmay ]; then
       echo "running Reflex c = ${c}"
-      ${time} -o reflex-${m}-${c}.time reflexCLI -i le10.json -s "c=${m}-${c}" || exit 1
+      ${time} -o reflex-${m}-${c}.time reflexCLI -i le10.json -s "c=${m}-${c}"
       grep "degrees of freedom" output/le10.log | awk '{printf("%g\t", $3)}' > reflex-${m}-${c}.sigmay
       jq .outputs.kpi_data[0].symm_tensor.data[0].yy output/le10_result_kpi.json >> reflex-${m}-${c}.sigmay
     fi
