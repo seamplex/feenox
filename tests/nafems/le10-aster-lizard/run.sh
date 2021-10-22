9bfa8db746a5bbcaf1c98daad209a9cbd029bf5f
gtheler <jeremy@seamplex.com>
2021-10-22 14:32:29 -0300

    check for termination

--- tests/nafems/le10-aster-lizard/run.sh
+++ tests/nafems/le10-aster-lizard/run.sh
@@ -44,8 +44,8 @@ fi
 
 # TODO: --check
 
-# for c in 1 0.5; do
-for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0.14 0.135 0.13 0.1275 0.125 0.1225 0.12 0.118 0.116 0.115 0.114 0.113 0.112 0.11 0.109 0.108 0.107 0.106 0.105 0.104 0.103 0.102 0.101 0.1; do
+for c in 1 0.5; do
+# for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0.14 0.135 0.13 0.1275 0.125 0.1225 0.12 0.118 0.116 0.115 0.114 0.113 0.112 0.11 0.109 0.108 0.107 0.106 0.105 0.104 0.103 0.102 0.101 0.1; do
 
  for m in hex tet; do
 
@@ -62,12 +62,15 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
     fi
     
     if [ -e feenox-${m}-${c}.sigmay ]; then
-      echo -ne "${c}\t" >> feenox-${m}.dat
-      cat feenox-${m}-${c}.sigmay | tr "\n" "\t" >> feenox-${m}.dat
-      cat feenox-${m}-${c}.time   | tr "\n" "\t" >> feenox-${m}.dat
-      echo >> feenox-${m}.dat
-    fi  
-  fi
+      grep terminated feenox-${m}-${c}.sigmay
+      if [ $? -e 0 ]; then
+        echo -ne "${c}\t" >> feenox-${m}.dat
+        cat feenox-${m}-${c}.sigmay | tr "\n" "\t" >> feenox-${m}.dat
+        cat feenox-${m}-${c}.time   | tr "\n" "\t" >> feenox-${m}.dat
+        echo >> feenox-${m}.dat
+      fi  
+    fi
+  fi  
 
   # ---- FeenoX (MUMPS) -----------------------------------------
   if [ ! -z "${has_feenox_mumps}" ]; then
@@ -77,10 +80,13 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
     fi
 
     if [ -e feenox_mumps-${m}-${c}.sigmay ]; then
-      echo -ne "${c}\t" >> feenox_mumps-${m}.dat
-      cat feenox_mumps-${m}-${c}.sigmay | tr "\n" "\t" >> feenox_mumps-${m}.dat
-      cat feenox_mumps-${m}-${c}.time   | tr "\n" "\t" >> feenox_mumps-${m}.dat
-      echo >> feenox_mumps-${m}.dat
+      grep terminated feenox_mumps-${m}-${c}.sigmay
+      if [ $? -ne 0 ]; then
+        echo -ne "${c}\t" >> feenox_mumps-${m}.dat
+        cat feenox_mumps-${m}-${c}.sigmay | tr "\n" "\t" >> feenox_mumps-${m}.dat
+        cat feenox_mumps-${m}-${c}.time   | tr "\n" "\t" >> feenox_mumps-${m}.dat
+        echo >> feenox_mumps-${m}.dat
+      fi  
     fi  
   fi
   
@@ -95,10 +101,13 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
     fi
     
     if [ -e sparselizard-${m}-${c}.sigmay ]; then
-      echo -ne "${c}\t" >> sparselizard-${m}.dat
-      cat sparselizard-${m}-${c}.sigmay | tr "\n" "\t" >> sparselizard-${m}.dat
-      cat sparselizard-${m}-${c}.time   | tr "\n" "\t" >> sparselizard-${m}.dat
-      echo >> sparselizard-${m}.dat
+      grep terminated sparselizard-${m}-${c}.sigmay
+      if [ $? -ne 0 ]; then
+        echo -ne "${c}\t" >> sparselizard-${m}.dat
+        cat sparselizard-${m}-${c}.sigmay | tr "\n" "\t" >> sparselizard-${m}.dat
+        cat sparselizard-${m}-${c}.time   | tr "\n" "\t" >> sparselizard-${m}.dat
+        echo >> sparselizard-${m}.dat
+      fi  
     fi  
   fi
   
@@ -109,6 +118,7 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
     if [ ! -e le10_2nd-${m}-${c}.unv ]; then
       gmsh -3 le10_2nd-${m}-${c}.msh -o le10_2nd-${m}-${c}.unv || exit 1
     fi
+    
     if [ ! -e aster-${m}-${c}.sigmay ]; then
       echo "running Aster c = ${c}"
       ln -sf le10_2nd-${m}-${c}.unv le10_2nd.unv
@@ -118,10 +128,13 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
     fi
 
     if [ -e aster-${m}-${c}.sigmay ]; then
-      echo -ne "${c}\t" >> aster-${m}.dat
-      cat aster-${m}-${c}.sigmay | tr "\n" "\t" >> aster-${m}.dat
-      cat aster-${m}-${c}.time   | tr "\n" "\t" >> aster-${m}.dat
-      echo >> aster-${m}.dat
+      grep terminated aster-${m}-${c}.sigmay
+      if [ $? -e 0 ]; then
+        echo -ne "${c}\t" >> aster-${m}.dat
+        cat aster-${m}-${c}.sigmay | tr "\n" "\t" >> aster-${m}.dat
+        cat aster-${m}-${c}.time   | tr "\n" "\t" >> aster-${m}.dat
+        echo >> aster-${m}.dat
+      fi  
     fi
   fi
 
@@ -133,10 +146,16 @@ for c in 1 0.75 0.5 0.4 0.35 0.3 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.145 0
       grep "degrees of freedom" output/le10.log | awk '{printf("%g\t", $3)}' > reflex-${m}-${c}.sigmay
       jq .outputs.kpi_data[0].symm_tensor.data[0].yy output/le10_result_kpi.json >> reflex-${m}-${c}.sigmay
     fi
-    echo -ne "${c}\t" >> reflex-${m}.dat
-    cat reflex-${m}-${c}.sigmay | tr "\n" "\t" >> reflex-${m}.dat
-    cat reflex-${m}-${c}.time   | tr "\n" "\t" >> reflex-${m}.dat
-    echo >> reflex-${m}.dat
+    
+    if [ -e aster-${m}-${c}.sigmay ]; then
+      grep terminated aster-${m}-${c}.sigmay
+      if [ $? -ne 0 ]; then
+        echo -ne "${c}\t" >> reflex-${m}.dat
+        cat reflex-${m}-${c}.sigmay | tr "\n" "\t" >> reflex-${m}.dat
+        cat reflex-${m}-${c}.time   | tr "\n" "\t" >> reflex-${m}.dat
+        echo >> reflex-${m}.dat
+      fi
+    fi
   fi
   
   
