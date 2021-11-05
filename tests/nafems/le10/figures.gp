cb87febad1784c681717c33c3c345a7763582145
gtheler <jeremy@seamplex.com>
2021-11-05 19:29:38 -0300

    separately plot tet & hex

--- tests/nafems/le10/figures.gp
+++ tests/nafems/le10/figures.gp
@@ -11,31 +11,31 @@ set xlabel "Mesh refinement factor"
 
 cy =  3
 set ylabel "Stress at D [MPa]"
-set output "sigmay-c.svg"
-load "plot-sigmay.gp"
+set output "sigmay-c-xxx.svg"
+load "plot-xxx-sigmay.gp"
 
 
 cy =  4
 set logscale y
 set ylabel "Wall time [sec]"
-set output "wall-c.svg"
-load "plot.gp"
+set output "wall-c-xxx.svg"
+load "plot-xxx.gp"
 
 
 cy =  5
 set ylabel "Kernel-space CPU time [sec]"
-set output "kernel-c.svg"
+set output "kernel-c-xxx.svg"
 replot
 
 cy =  6
 set ylabel "User-space CPU time [sec]"
-set output "user-c.svg"
+set output "user-c-xxx.svg"
 replot
 
 
 cy =  7
 set ylabel "Memory [kB]"
-set output "memory-c.svg"
+set output "memory-c-xxx.svg"
 replot
 
 
@@ -47,21 +47,21 @@ cy =  3
 set logscale x
 unset logscale y
 set ylabel "Stress at D [MPa]"
-set output "sigmay-dofs.svg"
-load "plot-sigmay.gp"
+set output "sigmay-dofs-xxx.svg"
+load "plot-xxx-sigmay.gp"
 
 cy =  4
 set logscale x
 set logscale y
 set ylabel "Wall time [sec]"
-set output "wall-dofs.svg"
-load "plot.gp"
+set output "wall-dofs-xxx.svg"
+load "plot-xxx.gp"
 
 cy =  5
 set logscale x
 set logscale y
 set ylabel "Kernel-space CPU time [sec]"
-set output "kernel-dofs.svg"
+set output "kernel-dofs-xxx.svg"
 replot
 
 
@@ -69,7 +69,7 @@ cy =  6
 set logscale x
 set logscale y
 set ylabel "User-space CPU time [sec]"
-set output "user-dofs.svg"
+set output "user-dofs-xxx.svg"
 replot
 
 
@@ -77,5 +77,5 @@ cy =  7
 set logscale x
 set logscale y
 set ylabel "Memory [kB]"
-set output "memory-dofs.svg"
+set output "memory-dofs-xxx.svg"
 replot
--- tests/nafems/le10/plot.sh
+++ tests/nafems/le10/plot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "${1}" ]; then 
+  echo "usage: $0 { tet | hex }"
+  exit 0  
+fi
+
 declare -A color
 color["feenox"]="black"
 color["sparselizard"]="blue"
@@ -23,24 +28,32 @@ lt["diagonal"]="5"
 lt["cholesky"]="6"
 lt["default"]="7"
 
-echo -n "plot " > plot.gp
+if [ "x$(ls *${1}*.dat | wc -l)" != "x0" ]; then
+  echo -n "plot " > plot-${1}.gp
 
-for i in $(ls *.dat | grep -v le10); do
-  program=$(echo $(basename ${i} .dat) | cut -d"_" -f 1)
-  solver=$(echo $(basename ${i} .dat) | cut -d"_" -f 2)
-  shape=$(echo $(basename ${i} .dat) | cut -d"_" -f 3)
- 
-  echo ${program} ${solver}
-  echo -n "\"${i}s\" u cx:cy  w lp  lw ${lw[$program]}  dashtype ${lt[$solver]}  lc \"${color[$program]}\"  ti \"${program} ${solver}\", " >> plot.gp
-  sort -r -g ${i} | grep -v nan | grep -v exited > ${i}s
-done
+  for i in feenox_*.dat sparselizard_*.dat aster_*.dat calculix_*.dat reflex_*.dat; do
+    program=$(echo $(basename ${i} .dat) | cut -d"_" -f 1)
+    solver=$(echo $(basename ${i} .dat) | cut -d"_" -f 2)
+    shape=$(echo $(basename ${i} .dat) | cut -d"_" -f 3)
+  
+    if [ "x${shape}" = "x${1}" ]; then
+      echo ${program} ${solver} ${shape}
+      echo -n "\"${i}s\" u cx:cy  w lp  lw ${lw[$program]}  dashtype ${lt[$solver]}  lc \"${color[$program]}\"  ti \"${program} ${solver}\", " >> plot-${1}.gp
+      sort -r -g ${i} | grep -v nan | grep -v exited > ${i}s
+    fi  
+  done
 
-cat plot.gp | tr -d '\n' > plot-sigmay.gp
-echo "-5.38 w l lw 1 lc \"tan1\" ti \"reference\"" >> plot-sigmay.gp
+  cat plot-${1}.gp | tr -d '\n' > plot-${1}-sigmay.gp
+  echo "-5.38 w l lw 1 lc \"tan1\" ti \"reference\"" >> plot-${1}-sigmay.gp
 
+  if [ -z "$(which gnuplot)" ]; then
+    echo "gnuplot not installed, not creating the plots, just the data files *.dats"
+  fi
 
-if [ -z "$(which gnuplot)" ]; then
-  echo "gnuplot not installed, not creating the plots, just the data files *.dats"
+  sed s/xxx/${1}/ figures.gp | gnuplot -
+  
+else
+
+  echo "nothing to plot for ${1}"
+  exit 0
 fi
-
-gnuplot figures.gp
