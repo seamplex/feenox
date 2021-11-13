8594471629d1c46796e97c8be5293d8d008d0bb2
gtheler <jeremy@seamplex.com>
2021-11-13 14:15:12 -0300

    get-remote

--- tests/nafems/le10/get-remote.sh
+++ tests/nafems/le10/get-remote.sh
@@ -7,12 +7,12 @@ fi
 
 if [[ -z "${2}" ]]; then
   scp ${1}:feenox/tests/nafems/le10/*.dat .
-  scp ${1}:feenox/tests/nafems/le10/*.md .
+  scp ${1}:feenox/tests/nafems/le10/arch*.md .
   scp ${1}:feenox/tests/nafems/le10/report*.md .
-  scp ${1}:feenox/tests/nafems/le10/report*.txt .
+  scp ${1}:feenox/tests/nafems/le10/*.txt .
 else
   scp -i ${2} ${1}:feenox/tests/nafems/le10/*.dat .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/arch*.md .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/report*.md .
-  scp -i ${2} ${1}:feenox/tests/nafems/le10/report*.txt .
+  scp -i ${2} ${1}:feenox/tests/nafems/le10/*.txt .
 fi
