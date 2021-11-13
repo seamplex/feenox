e856d6d83efda6c2f2f996f9ae7e4f686bd4ffd9
gtheler <jeremy@seamplex.com>
2021-11-13 14:16:36 -0300

    version

--- tests/nafems/le10/get-remote.sh
+++ tests/nafems/le10/get-remote.sh
@@ -9,10 +9,10 @@ if [[ -z "${2}" ]]; then
   scp ${1}:feenox/tests/nafems/le10/*.dat .
   scp ${1}:feenox/tests/nafems/le10/arch*.md .
   scp ${1}:feenox/tests/nafems/le10/report*.md .
-  scp ${1}:feenox/tests/nafems/le10/*.txt .
+  scp ${1}:feenox/tests/nafems/le10/version*.txt .
 else
   scp -i ${2} ${1}:feenox/tests/nafems/le10/*.dat .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/arch*.md .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/report*.md .
-  scp -i ${2} ${1}:feenox/tests/nafems/le10/*.txt .
+  scp -i ${2} ${1}:feenox/tests/nafems/le10/version*.txt .
 fi
