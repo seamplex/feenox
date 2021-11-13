fbb0c65df4658e46cd501e582b9c1eb249c67335
gtheler <jeremy@seamplex.com>
2021-11-13 19:37:29 -0300

    get svg

--- tests/nafems/le10/get-remote.sh
+++ tests/nafems/le10/get-remote.sh
@@ -10,9 +10,11 @@ if [[ -z "${2}" ]]; then
   scp ${1}:feenox/tests/nafems/le10/arch*.md .
   scp ${1}:feenox/tests/nafems/le10/report*.md .
   scp ${1}:feenox/tests/nafems/le10/version*.txt .
+  scp ${1}:feenox/tests/nafems/le10/*.svg .
 else
   scp -i ${2} ${1}:feenox/tests/nafems/le10/*.dat .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/arch*.md .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/report*.md .
   scp -i ${2} ${1}:feenox/tests/nafems/le10/version*.txt .
+  scp -i ${2} ${1}:feenox/tests/nafems/le10/*.svg .
 fi
