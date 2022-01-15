dee614ce27818e35a9d417253fe8c437b1fbd141
gtheler <jeremy@seamplex.com>
2022-01-15 16:09:10 -0300

    xfail tests that check for missing orthotropic properties

---
+++ tests/xfail-few-properties-ortho-young.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for i in . tests; do
+  if [ -e ${i}/functions.sh ]; then
+    . ${i}/functions.sh 
+  fi
+done
+if [ -z "${functions_found}" ]; then
+  echo "could not find functions.sh"
+  exit 1;
+fi
+
+checkpetsc
+
+answer xfail-few-properties-ortho-young.fee "0.41"
+exit $?
+
