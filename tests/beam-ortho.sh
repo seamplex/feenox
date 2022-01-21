9cf171cfa9d6d19af7c188fae42b2edead0571d7
gtheler <jeremy@seamplex.com>
2022-01-21 08:04:32 -0300

    updated orthotropic tests to remove ambiguous near-zero comparison

--- tests/beam-ortho.sh
+++ tests/beam-ortho.sh
@@ -15,18 +15,18 @@ checkpetsc
 # for i in beam-cantilever-*.geo; do gmsh -3 $i; done
 
 
-answer1 orthotropic-beam.fee tet4 "12.54 2.44 2.30 -2.44 0.26 -2.56"
+answer1 orthotropic-beam.fee tet4 "12.54 2.44 2.30 -2.44 -2.56"
 exitifwrong $?
 
-answer1 orthotropic-beam.fee tet10 "139.04 21.84 23.02 -2.13 6.81 -0.24"
+answer1 orthotropic-beam.fee tet10 "139.04 21.84 23.02 -2.13 -0.24"
 exitifwrong $?
 
-answer1 orthotropic-beam.fee hex8 "38.03 10.91 9.42 -1.16 0.00 -34.13"
+answer1 orthotropic-beam.fee hex8 "38.03 10.91 9.42 -1.16 -34.13"
 exitifwrong $?
 
-answer1 orthotropic-beam.fee hex20 "151.29 43.41 37.49 -6.92 -0.00 3.21"
+answer1 orthotropic-beam.fee hex20 "151.29 43.41 37.49 -6.92 3.21"
 exitifwrong $?
 
-answer1 orthotropic-beam.fee hex27 "158.92 45.60 39.39 -8.16 0.00 3.13"
+answer1 orthotropic-beam.fee hex27 "158.92 45.60 39.39 -8.16 3.13"
 exitifwrong $?
 
