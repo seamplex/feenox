343fc4193c83da6973bf582ea736cd855e611d9f
gtheler <jeremy@seamplex.com>
2022-02-21 07:18:56 -0300

    uo2 pellet test

---
+++ tests/uo2-pellet.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+for i in . tests; do
+  if [ -e ${i}/functions.sh ]; then
+    . ${i}/functions.sh 
+  fi
+done
+if [ -z "${functions_found}" ]; then
+  echo "could not find functions.sh"
+  exit 1;
+fi
+
+checkpetsc
+checkgmsh
+
+gmsh -3 ${dir}/pellet.geo
+
+# run with --ksp_view to see the difference
+answerzero pellet-linear.fee 1.5
+exitifwrong $?
+
+answerzero pellet-linear-guess.fee 1.5
+exitifwrong $?
+
+
+# run with --snes_view to see the difference
+answerzero pellet-nonlinear.fee 1117 2
+exitifwrong $?
+
+answerzero pellet-nonlinear-guess.fee 1117 2
+exitifwrong $?
+
+
+# transients
+answer pellet-linear-transient-from-ss.fee "30 1757"
+exitifwrong $?
+
+answer pellet-linear-transient-from-initial.fee "30 1277"
+exitifwrong $?
