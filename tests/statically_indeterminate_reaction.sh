d7459025f7762bc3dd0603a0424adfd65d9862e4
gtheler <jeremy@seamplex.com>
2022-08-25 15:05:39 -0300

    statically_indeterminate_reaction test

---
+++ tests/statically_indeterminate_reaction.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+for i in . tests; do
+  if [ -e ${i}/functions.sh ]; then
+    . ${i}/functions.sh 
+  fi
+done
+if [ -z "${functions_found}" ]; then
+  echo "could not find functions.sh"
+  exit 1;
+fi
+
+checkpde mechanical
+checkgmsh
+
+gmsh -3 ${dir}/statically_indeterminate_reaction.geo
+
+answerzero statically_indeterminate_reaction.fee 1e-2
+exitifwrong $?
