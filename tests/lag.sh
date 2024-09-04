b7043847dfe95f7a0ce0652b5787a3bf30ff7c45
gtheler <jeremy@seamplex.com>
2024-09-04 19:14:21 -0300

    raw TIME_PATH test

---
+++ tests/lag.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+for i in . tests; do
+  if [ -e ${i}/functions.sh ]; then
+    . ${i}/functions.sh 
+  fi
+done
+if [ -z "${functions_found}" ]; then
+  echo "could not find functions.sh"
+  exit 1;
+fi
+
+answer lag.fee "0.16"
+exitifwrong $?
+
+answer lag_compact.fee "0.16"
+exitifwrong $?
+
+# check TIME_PATH
+echo -n "time_path_raw.fee ... "
+
+if [ "x$(${feenox} ${dir}/time_path_sundials.fee | wc -l)" != "x5" ]; then
+  echo "failed"
+  return 1
+fi
+echo "ok"
