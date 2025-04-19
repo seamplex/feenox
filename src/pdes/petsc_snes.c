b333e130a7767c67e9fbfb5ee9ec33afa5ca07da
jeremy theler <jeremy@seamplex.com>
2025-04-19 11:41:19 -0300

    fix for petsc < 3.22

--- src/pdes/petsc_snes.c
+++ src/pdes/petsc_snes.c
@@ -24,7 +24,7 @@
 int feenox_problem_solve_petsc_nonlinear(void) {
 
 #ifdef HAVE_PETSC
-  
+
   if (feenox.pde.snes == NULL) {
     // TODO: move to a separate function
     petsc_call(SNESCreate(PETSC_COMM_WORLD, &feenox.pde.snes));
@@ -36,15 +36,15 @@ int feenox_problem_solve_petsc_nonlinear(void) {
     petsc_call(SNESSetFunction(feenox.pde.snes, feenox.pde.r, feenox_snes_residual, NULL));
     feenox_check_alloc(feenox.pde.J_snes = feenox_problem_create_matrix("J_snes"));
     petsc_call(SNESSetJacobian(feenox.pde.snes, feenox.pde.J_snes, feenox.pde.J_snes, feenox_snes_jacobian, NULL));    
-    
+
     // TODO
     feenox_call(feenox_problem_setup_snes(feenox.pde.snes));
-  }  
-  
+  }
+
   // solve
   feenox.pde.progress_last = 0; // reset the progress bar counter
   petsc_call(SNESSolve(feenox.pde.snes, NULL, feenox.pde.phi));
-  
+
   // check convergence
   SNESConvergedReason reason;
   petsc_call(SNESGetConvergedReason(feenox.pde.snes, &reason));
@@ -53,8 +53,7 @@ int feenox_problem_solve_petsc_nonlinear(void) {
     // TODO: go deeper into the KSP and PC
     return FEENOX_ERROR;
   }
-  
-  
+
   // finish the progress line
   if (feenox.pde.progress_ascii == PETSC_TRUE) {
     int i;
@@ -66,18 +65,14 @@ int feenox_problem_solve_petsc_nonlinear(void) {
     if (feenox.mpi_rank == 0) {
       printf("\n");
       fflush(stdout);
-    }  
+    }
   }
-  
-#endif  
+#endif
   return FEENOX_OK;
-  
 }
 
 #ifdef HAVE_PETSC
 int feenox_problem_setup_snes(SNES snes) {
-  
-  
   // TODO: have an explicit default
   // TODO: set the line search
   if (feenox.pde.snes_type != NULL) {
@@ -99,20 +94,23 @@ int feenox_problem_setup_snes(SNES snes) {
                                      feenox_var_value(feenox.pde.vars.snes_stol),
                                      (PetscInt)feenox_var_value(feenox.pde.vars.snes_max_it),
                                      PETSC_DEFAULT));
-  
+#if PETSC_VERSION_LT(3,22,0)
+  petsc_call(SNESSetDivergenceTolerance(snes, -1));
+#else
   petsc_call(SNESSetDivergenceTolerance(snes, PETSC_UNLIMITED));
-  
+#endif
+
   petsc_call(SNESSetFromOptions(snes));
   // TODO: this call complains about DM (?) in thermal-slab-transient.fee 
   // petsc_call(SNESSetUp(snes));
-  
+
   // customize ksp (and pc)---this needs to come after setting the jacobian
   KSP ksp;
   petsc_call(SNESGetKSP(snes, &ksp));
   feenox_call(feenox_problem_setup_ksp(ksp));
 
   return FEENOX_OK;
-}  
+}
 
 PetscErrorCode feenox_snes_residual(SNES snes, Vec phi, Vec r, void *ctx) {
   // set dirichlet BCs on the solution
@@ -126,8 +124,7 @@ PetscErrorCode feenox_snes_residual(SNES snes, Vec phi, Vec r, void *ctx) {
   // build both the residual and the jacobian
   feenox_call(feenox_problem_phi_to_solution(feenox.pde.phi_bc));
   feenox_call(feenox_problem_build());
-  
-  
+
   if (feenox.pde.has_internal_fluxes) {
     // if the problem provides an internal flux, use it
     petsc_call(VecCopy(feenox.pde.f, r));
@@ -135,31 +132,30 @@ PetscErrorCode feenox_snes_residual(SNES snes, Vec phi, Vec r, void *ctx) {
     // otherwise make it up from the stiffness and the solution
     petsc_call(MatMult(feenox.pde.K, feenox.pde.phi_bc, r));
   }
-  
+
 //  printf("r as in f\n");
 //  petsc_call(VecView(r, PETSC_VIEWER_STDOUT_SELF));
-  
+
   // subtract b
   petsc_call(VecAXPY(r, -1.0, feenox.pde.b));
 //  printf("r as in f-b\n");
 //  VecView(r, PETSC_VIEWER_STDOUT_SELF);
-  
+
   // set dirichlet BCs on the residual
   feenox_call(feenox_problem_dirichlet_set_r(r, phi));
-  
+
 //  printf("r as in f-b + bcs\n");
 //  VecView(r, PETSC_VIEWER_STDOUT_SELF);
-  
+
 //  printf("solution\n");
 //  VecView(phi, PETSC_VIEWER_STDOUT_SELF);
-  
 
   return FEENOX_OK;
 }
 
 
 PetscErrorCode feenox_snes_jacobian(SNES snes,Vec phi, Mat J_snes, Mat P, void *ctx) {
-  
+
   // J_snes = (K + JK - Jb)_bc
   // TODO: we want SAME_NONZERO_PATTERN!
   petsc_call(MatAssemblyBegin(feenox.pde.K, MAT_FINAL_ASSEMBLY));
@@ -170,30 +166,29 @@ PetscErrorCode feenox_snes_jacobian(SNES snes,Vec phi, Mat J_snes, Mat P, void *
 
 //  printf("K\n");
 //  MatView(feenox.pde.K, PETSC_VIEWER_STDOUT_SELF);
-  
+
 //  printf("JK\n");
 //  MatView(feenox.pde.JK, PETSC_VIEWER_STDOUT_SELF);
-  
+
   if (feenox.pde.has_jacobian_K) {
     petsc_call(MatAXPY(J_snes, +1.0, feenox.pde.JK, DIFFERENT_NONZERO_PATTERN));
-  }  
-  
+  }
+
   if (feenox.pde.has_jacobian_b) {
     petsc_call(MatAXPY(J_snes, -1.0, feenox.pde.Jb, DIFFERENT_NONZERO_PATTERN));
   }
-  
+
   feenox_call(feenox_problem_dirichlet_set_J(J_snes));
-  
+
 //  printf("jacobian\n");
 //  MatView(J_snes, PETSC_VIEWER_STDOUT_SELF);
-  
+
   return FEENOX_OK;
 }
 
 
 PetscErrorCode feenox_snes_monitor(SNES snes, PetscInt n, PetscReal rnorm, void *dummy) {
-  
-  
+
   if (feenox.pde.progress_r0 == 0) {
     feenox.pde.progress_r0 = rnorm;
   }
@@ -201,7 +196,7 @@ PetscErrorCode feenox_snes_monitor(SNES snes, PetscInt n, PetscReal rnorm, void 
   double current_progress = (rnorm > 1e-20) ? log((rnorm/feenox.pde.progress_r0))/log(feenox_var_value(feenox.pde.vars.ksp_rtol)) : 1;
   if (current_progress > 1) {
     current_progress = 1;
-  } 
+  }
 
   if (feenox.pde.progress_ascii == PETSC_TRUE) {
     size_t i = 0;
